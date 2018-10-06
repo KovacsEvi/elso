87e1293084acfdc9d659e3f1d3779b3fe53b98bc
kovevi <kovevi@gmail.com>
2018-10-06 11:20:19 +0200

    összeadás ilyesmi kész

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -10,13 +10,16 @@ public class Main {
 			int a=Integer.parseInt(args[0]);
 			int b=Integer.parseInt(args[1]);
 			int c=Integer.parseInt(args[2]);
-			
-			
+			int szorzat=a*b;
+			double hanyados=(double) c/b;
 			System.out.println("térfogat= "+a*b*c);
 			
 			System.out.println("felszín= "+2*(a*b+b*c+a*c));
 			
 			System.out.println("összeg: "+(a+b));
+			System.out.println("különbség: "+(a+b));
+			System.out.println("szorzat: "+szorzat);
+			System.out.println("hanyados: "+hanyados);
 			
 			
 		
