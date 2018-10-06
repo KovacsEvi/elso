403887e33235b39186544f930e0d91ab921ffc57
kovevi <kovevi@gmail.com>
2018-10-06 10:58:19 +0200

    init

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	/**
+	 * Eza metódus azért felelős, hogy...
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=Integer.parseInt(args[0]);
+			int b=Integer.parseInt(args[1]);
+			int c=Integer.parseInt(args[2]);
+			
+			
+			System.out.println("térfogat= "+a*b*c);
+			System.out.println("felszín= "+2*(a*b+b*c+a*c));
+			
+			System.out.println("összeg: "+(a+b));
+			
+			
+		
+	}
+
+}
